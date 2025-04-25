d136b0c93ef59acf60e8ddd7f1839b0fc3964eac
Ohin-Kyuu <ohin.kyuu@gmail.com>
2025-04-25 21:00:13 +0800

    Added: Vision Center- Score prediction

---
+++ packages/center-pkg/center_score/importClass/callback.py
@@ -0,0 +1,88 @@
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped, PoseArray
+from std_msgs.msg import Int16
+import threading
+
+class SensorCallback(Node):
+    def __init__(self):
+        super().__init__('sensor_callback_node')
+        self.get_logger().info(f'Initializing {self.get_name()}...')
+        self._init_state()
+        self._init_subscriptions()
+        self.get_logger().info(f'{self.get_name()} is up.')
+
+    def _init_state(self):
+        self.get_logger().info('Initializing state variables...')
+        self.lock = threading.Lock()
+        self.last_superstar_pose_ = None
+        self.last_sima_pose_array_ = None
+        self.last_layer_ = None
+        self.last_robot_pose_ = None
+        self.last_platform_pose_array_ = None
+
+    def _init_subscriptions(self):
+        self.get_logger().info('Creating subscriptions...')
+
+        self.create_subscription(
+            PoseStamped,
+            '/vision/aruco/sima4/average_pose',
+            self.superstar_callback,
+            10
+        )
+
+        self.create_subscription(
+            PoseArray,
+            '/vision/aruco/sima_poseArray',
+            self.sima_callback,
+            10
+        )
+
+        self.create_subscription(
+            Int16,
+            '/vision/aruco/onboard/layer',
+            self.layer_callback,
+            10
+        )
+
+        self.create_subscription(
+            PoseStamped,
+            '/vision/aruco/robot/average_pose',
+            self.robot_callback,
+            10
+        )
+
+        self.create_subscription(
+            PoseArray,
+            '/detected/global_center_poses/platform',
+            self.platform_callback,
+            10
+        )
+    def superstar_callback(self, msg: PoseStamped):
+        with self.lock:
+            self.last_superstar_pose_ = msg
+
+    def sima_callback(self, msg: PoseArray):
+        with self.lock:
+            self.last_sima_pose_array_ = msg
+
+    def layer_callback(self, msg: Int16):
+        with self.lock:
+            self.last_layer_ = msg
+
+    def robot_callback(self, msg: PoseStamped):
+        with self.lock:
+            self.last_robot_pose_ = msg
+
+    def platform_callback(self, msg: PoseArray):
+        with self.lock:
+            self.last_platform_pose_array_ = msg
+
+    def get_sensor_data(self):
+        with self.lock:
+            return {
+                'superstar_pose': self.last_superstar_pose_,
+                'sima_pose_array': self.last_sima_pose_array_,
+                'layer': self.last_layer_,
+                'robot_pose': self.last_robot_pose_,
+                'platform_pose_array': self.last_platform_pose_array_
+            }
---
+++ packages/center-pkg/center_score/importClass/region_logic.py
@@ -0,0 +1,149 @@
+import cv2
+import numpy as np
+
+SIMA_REGION = {
+    'sima_1': {
+        'points': np.array([[1.8, 1.2], [1.65, 1.55], 
+                            [1.95, 1.55], [1.95, 1.6], 
+                            [2.3, 1.45], [2.3, 1.6]]),
+    },
+    'sima_2': {
+        'points': np.array([[1.25, 1.55], [1.75, 1.55], 
+                            [1.15, 1.2], [1.85, 1.2]]),
+    },
+    'sima_3': {
+        'points': np.array([[1.05, 1.55], [1.35, 1.55], 
+                            [1.05, 1.6], [0.75, 1.6],
+                            [1.45, 1.6], [1.2, 1.2]]),
+    },
+}
+
+HOME_REGION = {
+    'x_min': 2.40,
+    'x_max': 2.85,
+    'y_min': 1.55,
+    'y_max': 2.00,
+}
+
+BUILD_REGION = {
+    'build_1': {
+        'x_min': 0.00,
+        'x_max': 0.45,
+        'y_min': 0.65,
+        'y_max': 1.10,
+    },
+    'build_2': {
+        'x_min': 0.00,
+        'x_max': 0.45,
+        'y_min': 0.00,
+        'y_max': 0.15,
+    },
+    'build_3': {
+        'x_min': 1.55,
+        'x_max': 2.00,
+        'y_min': 0.00,
+        'y_max': 0.45,
+    },
+    'build_4': {
+        'x_min': 2.00,
+        'x_max': 2.45,
+        'y_min': 0.00,
+        'y_max': 0.15,
+    },
+}
+
+STAGE_REGION = {
+    'x_min': 1.05,
+    'x_max': 1.95,
+    'y_min': 1.40,
+    'y_max': 1.55,
+}
+
+class RegionLogic():
+    def __init__(self, sensor_node):
+        self.sensor_node = sensor_node
+
+    def is_point_in_polygon(self, point, polygon_points):
+        polygon_points = polygon_points.astype(np.float32)
+        result = cv2.pointPolygonTest(polygon_points, point, False)
+        return result >= 0
+
+    def check_superstar_pose_in_region(self):
+        superstar_point = 0
+        data = self.sensor_node.get_sensor_data()
+        superstar_pose = data['superstar_pose']
+
+        if superstar_pose is None:
+            return 0
+
+        x = superstar_pose.pose.position.x
+        y = superstar_pose.pose.position.y
+
+        if STAGE_REGION['x_min'] <= x <= STAGE_REGION['x_max'] and \
+           STAGE_REGION['y_min'] <= y <= STAGE_REGION['y_max']:
+            superstar_point += 15
+
+        return superstar_point
+
+    def check_sima_pose_in_region(self):
+        sima_point = 0
+        region_hit_count = 0
+        data = self.sensor_node.get_sensor_data()
+        sima_pose = data['sima_pose_array']
+
+        if sima_pose is None or not sima_pose.poses:
+            return 0
+
+        for sima_id, region in SIMA_REGION.items():
+            polygon_points = region['points']
+            for pose in sima_pose.poses:
+                point = (pose.position.x, pose.position.y)
+                if self.is_point_in_polygon(point, polygon_points):
+                    sima_point += 5
+                    region_hit_count += 1
+                    break
+        
+        if region_hit_count == 3:
+            sima_point += 10
+
+        return sima_point
+
+    def check_robot_pose_in_region(self):
+        data = self.sensor_node.get_sensor_data()
+        robot_pose = data['robot_pose']
+        
+        if robot_pose is None:
+            return 0
+
+        x = robot_pose.pose.position.x
+        y = robot_pose.pose.position.y
+
+        if HOME_REGION['x_min'] <= x <= HOME_REGION['x_max'] and \
+           HOME_REGION['y_min'] <= y <= HOME_REGION['y_max']:
+            return 10
+        return 0
+
+    def check_platform_pose_in_region(self):
+        build_point = 0
+        data = self.sensor_node.get_sensor_data()
+        platform_pose = data['platform_pose_array']
+        
+        if platform_pose is None or not platform_pose.poses:
+            return 0
+
+        for build_id, region in BUILD_REGION.items():
+            for pose in platform_pose.poses:
+                x = pose.position.x
+                y = pose.position.y
+                z = pose.position.z
+                if region['x_min'] <= x <= region['x_max'] and \
+                    region['y_min'] <= y <= region['y_max']:
+                    
+                    if z > 0.3:
+                        build_point += 28
+                    elif z > 0.1:
+                        build_point += 12
+                    elif z > 0:
+                        build_point += 4
+
+        return build_point
